44a1663d1b78e4335074193cd9a3747ab235d2f3
accforsomething <testletterboxforsemething@rambler.ru>
2022-07-04 13:30:09 +0300

    initial commit

---
+++ src/main/java/com/example/crowlerdemo/CrowlerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.crowlerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowlerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowlerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crowlerdemo/CrowlerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crowlerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowlerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
